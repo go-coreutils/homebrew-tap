b1089c20ae32a4908c0859d513d0e6c691a17e4b
Kevin C. Krinke <kevin@krinke.ca>
2024-08-10 18:30:42 -0400

    initial commit

---
+++ Formula/eheditor.rb
@@ -0,0 +1,29 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Eheditor < Formula
+  desc "Manage the OS /etc/hosts file."
+  homepage "https://github.com/go-coreutils/etc-hosts-editor"
+  license "Apache-2.0"
+  head "https://github.com/go-coreutils/etc-hosts-editor.git", branch: "release-v0.7.1"
+
+  depends_on "go" => :build
+
+  def install
+    ENV.deparallelize  # if your formula fails when building in parallel
+    system "make", "prefix=#{prefix}", "prefix_etc=#{prefix}/etc", "release", "install", "install-autocomplete"
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test enjenv`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system bin/"eheditor", "--help"
+  end
+end
---
+++ README.md
@@ -0,0 +1,46 @@
+# Go-CoreUtils Homebrew Tap
+
+## Summary
+
+This [homebrew] [tap] provides the [Go-CoreUtils] command line utilities.
+
+## Formula
+
+### strcaseto
+
+Simple command line helper that converts strings to specific strcases such as
+CamelCase and SCREAMING-KEBAB.
+
+### replace
+
+Search and replace text in files and can output a diff of all changes made
+(or would be made).
+
+Note: conflicts with the existing `rpl` homebrew formula
+
+### eheditor
+
+Manage the OS /etc/hosts file.
+
+### gassc
+
+Go sass compiler.
+
+### go-mod-update
+
+Go mod updater.
+
+## Installation
+
+`brew install go-coreutils/tap/<formula>`
+
+Or `brew tap go-coreutils/tap` and then `brew install <formula>`.
+
+## Documentation
+
+`brew help`, `man brew` or check the [documentation].
+
+[homebrew]: https://brew.sh
+[tap]: https://docs.brew.sh/Taps
+[Go-CoreUtils]: https://github.com/go-coreutils
+[documentation]: https://docs.brew.sh
